041f3e9a0672a6aeff57be940a161b0f4c09fb50
yusuf <ysfozturk3@gmail.com>
2021-10-20 20:42:01 +0200

    completed storyboard objects

---
+++ ArtBook_CoreData/DetailsVC.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsVC.swift
+//  ArtBook_CoreData
+//
+//  Created by ysf on 20.10.21.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
+
+}
