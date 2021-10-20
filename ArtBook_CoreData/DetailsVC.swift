5ee3da9fee99d5e587668c473eb130b0608ef2dc
yusuf <ysfozturk3@gmail.com>
2021-10-20 23:38:27 +0200

    completed save button functioin

--- ArtBook_CoreData/DetailsVC.swift
+++ ArtBook_CoreData/DetailsVC.swift
@@ -49,8 +49,33 @@ class DetailsVC: UIViewController, UIImagePickerControllerDelegate,UINavigationC
     
 
     @IBAction func saveButtonClicked(_ sender: Any) {
+        // dosyalar icerisinde bulunan appDelegate e ve icerisindeki context fonksiyonuna ulastik
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
+        let newPainting = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
         
+        //Attributes
+        
+        newPainting.setValue(nameText.text!, forKey: "name")
+        newPainting.setValue(artistText.text!, forKey: "artist")
+        
+        if let year = Int(yearText.text!){
+            newPainting.setValue(year, forKey: "year")
+        }
+        
+        newPainting.setValue(UUID(), forKey: "id")
+        
+        let data = imageView.image!.jpegData(compressionQuality: 0.4)
+        newPainting.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("success")
+        } catch  {
+            print("error")
+
+        }
     }
     
 
